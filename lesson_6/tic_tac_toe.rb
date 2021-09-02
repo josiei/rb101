4eb3893ed4a34e47f82719984ec9e0f1623e5f75
Jocelyn Icaza <jocelyn.icaza@gmail.com>
2021-09-02 12:45:43 -0700

    tic tac toe + bonus features

--- lesson_6/tic_tac_toe.rb
+++ lesson_6/tic_tac_toe.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -62,24 +64,91 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
-def computer_places_piece!(brd)
-  square = ''
-  WINNING_LINES.each do |line|
-    if brd.values_at(*line).count(COMPUTER_MARKER) == 2
-      if brd[line[0]] == INITIAL_MARKER
-        square = line[0]
-        break
-      elsif brd[line[1]] == INITIAL_MARKER
-        square = line[1]
-        break
-      elsif brd[line[2]] == INITIAL_MARKER
-        square = line[2]
-        break
-      end
-    else
-      square = empty_squares(brd).sample
-    end
+def place_piece!(brd, crnt_player)
+  if crnt_player == "player"
+    player_places_piece!(brd)
+  elsif crnt_player == "computer"
+    computer_places_piece!(brd)
+  end
+end
 
+def alternate_player(crnt_player)
+  if crnt_player == "player"
+    "computer"
+  elsif crnt_player == "computer"
+    "player"
+  end
+end
+
+def player_chooses_first
+  prompt("Who would you like to go first?")
+  answer = gets.chomp.downcase
+  if answer.start_with?("p")
+    "player"
+  elsif answer.start_with?("c")
+    "computer"
+  else
+    computer_chooses_first
+  end
+end
+
+def computer_chooses_first
+  options = ["player", "computer"]
+  answer = options.sample
+end
+
+def who_goes_first()
+  first_player = nil
+  prompt("Player picks or Computer picks?")
+  user_input = gets.chomp
+
+  if user_input.downcase.start_with?("p")
+    first_player = player_chooses_first
+  elsif user_input.downcase.start_with?("c")
+    first_player = computer_chooses_first  
+  end
+  first_player
+end
+
+def find_at_risk_square(line, brd)
+  if brd.values_at(*line).count(PLAYER_MARKER) == 2
+    brd.select { |k,v| line.include?(k) && v == INITIAL_MARKER}.keys.first
+  else
+    nil
+  end
+end
+
+def find_attack_square(line, brd)
+  if brd.values_at(*line).count(COMPUTER_MARKER) == 2
+    brd.select { |k,v| line.include?(k) && v == INITIAL_MARKER}.keys.first
+  else
+    nil
+  end
+end
+
+def computer_places_piece!(brd)
+  square = nil
+
+  WINNING_LINES.each do |line|
+    square = find_attack_square(line, brd)
+    break if square
+  end
+
+  if !square
+    WINNING_LINES.each do |line|
+      square = find_at_risk_square(line, brd)
+      break if square
+    end
+  end
+
+  if !square
+    if empty_squares(brd).include?(5)
+      square = 5
+    end
+  end
+
+  if !square
+    square = empty_squares(brd).sample
   end
 
   brd[square] = COMPUTER_MARKER
@@ -140,6 +209,7 @@ end
 
 player_score = 0
 computer_score = 0
+current_player = who_goes_first
 
 loop do
   board = initialize_board
@@ -147,11 +217,8 @@ loop do
 
   loop do
     display_board(board)
-
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-
-    computer_places_piece!(board)
+    place_piece!(board, current_player)
+    current_player = alternate_player(current_player)
     break if someone_won?(board) || board_full?(board)
   end
 
